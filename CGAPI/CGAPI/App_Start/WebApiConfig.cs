8e6b28136a702e1589bcca926671c0a18731c176
HenryM8 <henryrm8@gmail.com>
2024-06-20 16:24:01 -0600

    Project base-Dependencies-Libraries-Initial structure-Model

---
+++ CGAPI/CGAPI/App_Start/WebApiConfig.cs
@@ -0,0 +1,26 @@
+﻿using CGAPI.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace CGAPI
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Configuración y servicios de Web API
+            config.MessageHandlers.Add(new TokenValidationHandler());
+
+            // Rutas de Web API
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ CGAPI/CGAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace CGAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
