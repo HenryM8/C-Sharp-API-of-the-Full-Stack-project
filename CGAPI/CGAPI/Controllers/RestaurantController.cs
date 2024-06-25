1dddded2b2c731ec9bbe82b19ad147f603f6795f
HenryM8 <henryrm8@gmail.com>
2024-06-25 11:55:26 -0600

    Controllers for users and restaurants

---
+++ CGAPI/CGAPI/Controllers/RestaurantController.cs
@@ -0,0 +1,195 @@
+﻿using CGAPI.Entities;
+using CGAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Core.Metadata.Edm;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace CGAPI.Controllers
+{
+    [Authorize]
+    public class RestaurantController : ApiController
+    {
+        [HttpPost]
+        [Route("api/AddRestaurant")]
+        public long RegistrarRestaurants(RestaurantEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                Restaurants tabla = new Restaurants();
+                tabla.restaurant_name = entidad.Restaurant_name;
+                tabla.address = entidad.Address;
+                tabla.phone_number = entidad.Phone_number;
+                tabla.website = entidad.Website;
+                tabla.url_image = entidad.Url_image;
+                tabla.description = entidad.Description;
+                tabla.location_map = entidad.Location_map;
+                tabla.is_visible = entidad.Is_visible;
+                bd.Restaurants.Add(tabla);
+                bd.SaveChanges();
+
+                return tabla.restaurant_id;
+            }
+        }
+
+        [HttpGet]
+        [Route("api/GetRestaurants")]
+        public List<RestaurantEnt> GetRestaurants()
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Restaurants
+                             join r in bd.Reviews on x.restaurant_id equals r.restaurant_id into Union
+                             from r in Union.DefaultIfEmpty()
+                             select new
+                             {
+                                 x.restaurant_id,
+                                 x.restaurant_name,
+                                 x.address,
+                                 x.phone_number,
+                                 x.website,
+                                 x.url_image,
+                                 x.description,
+                                 x.is_visible,
+                                 x.location_map,
+                                 reviews_count = x.Reviews.Count(),
+                                 stars_count = x.Reviews.Sum(s => s.rating) != null ? x.Reviews.Sum(s => s.rating) / x.Reviews.Count() : 0
+                             }
+                             ).Distinct().ToList();
+
+                List<RestaurantEnt> resp = new List<RestaurantEnt>();
+
+                if (datos.Count > 0)
+                {
+                    foreach (var item in datos)
+                    {
+                        resp.Add(new RestaurantEnt
+                        {
+                            Restaurant_id = item.restaurant_id,
+                            Restaurant_name = item.restaurant_name,
+                            Address = item.address,
+                            Phone_number = item.phone_number,
+                            Website = item.website,
+                            Url_image = item.url_image,
+                            Description = item.description,
+                            Is_visible = (bool)item.is_visible,
+                            Location_map = item.location_map,
+                            Reviews_count = item.reviews_count,
+                            Stars_count = item.stars_count,
+                        });
+                    }
+                }
+
+                return resp;
+            }
+        }
+
+        [HttpGet]
+        [Route("api/GetRestaurant")]
+        public RestaurantEnt GetRestaurant(long q)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Restaurants
+                             join r in bd.Reviews on x.restaurant_id equals r.restaurant_id into Union
+                             from r in Union.DefaultIfEmpty()
+                             where x.restaurant_id == q
+                             select new
+                             {
+                                 x.restaurant_id,
+                                 x.restaurant_name,
+                                 x.address,
+                                 x.phone_number,
+                                 x.website,
+                                 x.url_image,
+                                 x.description,
+                                 x.is_visible,
+                                 x.location_map,
+                                 stars_count = x.Reviews.Sum(s => s.rating) != null ? x.Reviews.Sum(s => s.rating) / x.Reviews.Count() : 0
+                             }).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    RestaurantEnt resp = new RestaurantEnt();
+                    resp.Restaurant_id = datos.restaurant_id;
+                    resp.Restaurant_name = datos.restaurant_name;
+                    resp.Address = datos.address;
+                    resp.Phone_number = datos.phone_number;
+                    resp.Website = datos.website;
+                    resp.Url_image = datos.url_image;
+                    resp.Description = datos.description;
+                    resp.Is_visible = (bool)datos.is_visible;
+                    resp.Location_map = datos.location_map;
+                    resp.Stars_count = datos.stars_count;
+                    return resp;
+                }
+
+                return null;
+            }
+        }
+
+        [HttpPut]
+        [Route("api/ChangeRestaurantVisibility")]
+        public int ChangeRestaurantVisibility(RestaurantEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Restaurants
+                             where x.restaurant_id == entidad.Restaurant_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.is_visible = !datos.is_visible;
+                    return bd.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        [HttpPut]
+        [Route("api/ValidateRestaurantData")]
+        public int ValidateUserData(RestaurantEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Restaurants
+                             where x.restaurant_id == entidad.Restaurant_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.restaurant_name = entidad.Restaurant_name;
+                    datos.address = entidad.Address;
+                    datos.phone_number = entidad.Phone_number;
+                    datos.description = entidad.Description;
+                    datos.website = entidad.Website;
+                    datos.location_map = entidad.Location_map;
+                    return bd.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        [HttpPut]
+        [Route("api/UpdateRestaurantImage")]
+        public void UpdateRestaurantImage(RestaurantEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Restaurants
+                             where x.restaurant_id == entidad.Restaurant_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.url_image = entidad.Url_image;
+                    bd.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ CGAPI/CGAPI/Controllers/UserController.cs
@@ -0,0 +1,262 @@
+﻿using CGAPI.App_Start;
+using CGAPI.Entities;
+using CGAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+
+namespace CGAPI.Controllers
+{
+    [Authorize]
+    public class UserController : ApiController
+    {
+        generals model = new generals();
+        TokenGenerator tokenGenerator = new TokenGenerator();
+
+        [HttpPost]
+        [Route("api/AddUser")]
+        [AllowAnonymous]
+        public int AddUser(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                Users tabla = new Users();
+                tabla.mail = entidad.Mail;
+                tabla.password = entidad.Password;
+                tabla.username = entidad.Username;
+                tabla.url_image = entidad.Url_image;
+                tabla.rol_id = entidad.Rol_id;
+                tabla.register_date = entidad.Register_date;
+                tabla.date_recovery = entidad.Date_recovery;
+                tabla.use_recovery_password = entidad.Use_recovery_password;
+                bd.Users.Add(tabla);
+                return bd.SaveChanges();
+            }
+        }
+
+        [HttpGet]
+        [Route("api/GetUsers")]
+        public List<UserEnt> GetUsers()
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from u in bd.Users
+                             join r in bd.Roles on u.rol_id equals r.rol_id
+                             select new
+                             {
+                                 u.user_id,
+                                 u.mail,
+                                 u.username,
+                                 u.url_image,
+                                 u.rol_id,
+                                 u.register_date,
+                                 r.rol_name,
+                                 reviews = u.Reviews.Count(),
+                             }).ToList();
+                List<UserEnt> resp = new List<UserEnt>();
+
+                if (datos.Count > 0)
+                {
+                    foreach (var item in datos)
+                    {
+                        resp.Add(new UserEnt
+                        {
+                            User_id = item.user_id,
+                            Mail = item.mail,
+                            Username = item.username,
+                            Url_image = item.url_image,
+                            Rol_id = item.rol_id,
+                            Register_date = (DateTime)item.register_date,
+                            Rol_name = item.rol_name,
+                            Reviews = item.reviews,
+                        });
+                    }
+                }
+
+                return resp;
+            }
+        }
+
+        [HttpGet]
+        [Route("api/GetUser")]
+        public UserEnt GetUser(long q)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Users
+                             where x.user_id == q
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    UserEnt resp = new UserEnt();
+                    resp.User_id = datos.user_id;
+                    resp.Mail = datos.mail;
+                    resp.Username = datos.username;
+                    resp.Url_image = datos.url_image;
+                    resp.Rol_id = datos.rol_id;
+                    resp.Register_date = (DateTime)datos.register_date;
+                    return resp;
+                }
+
+                return null;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/Login")]
+        [AllowAnonymous]
+        public UserEnt Login(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Users
+                             join r in bd.Roles on x.rol_id equals r.rol_id
+                             where x.username == entidad.Username
+                             && x.password == entidad.Password
+                             select new
+                             {
+                                 x.user_id,
+                                 x.username,
+                                 x.mail,
+                                 x.url_image,
+                                 x.rol_id,
+                                 x.register_date,
+                                 x.date_recovery,
+                                 x.use_recovery_password,
+                                 r.rol_name
+                             }).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    // Asegúrate de que use_recovery_password tenga un valor antes de compararlo con true
+                    if (datos.use_recovery_password.HasValue && datos.use_recovery_password.Value && datos.date_recovery < DateTime.Now)
+                    {
+                        return null;
+                    }
+
+                    UserEnt resp = new UserEnt();
+                    resp.User_id = datos.user_id;
+                    resp.Username = datos.username;
+                    resp.Mail = datos.mail;
+                    resp.Url_image = datos.url_image;
+                    resp.Rol_id = datos.rol_id;
+                    resp.Register_date = (DateTime)datos.register_date;
+                    resp.Rol_name = datos.rol_name;
+                    resp.Token = tokenGenerator.GenerateTokenJwt(datos.mail);
+                    return resp;
+                }
+
+                return null;
+            }
+        }
+
+
+        [HttpPut]
+        [Route("api/ValidateUserData")]
+        public int ValidateUserData(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Users
+                             where x.user_id == entidad.User_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.url_image = entidad.Url_image;
+                    datos.username = entidad.Username;
+                    datos.mail = entidad.Mail;
+                    return bd.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        [HttpPost]
+        [Route("api/RecoveryPassword")]
+        [AllowAnonymous]
+        public bool RecoveryPassword(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                //el usuario es válido?
+                var datos = (from x in bd.Users
+                             where x.mail == entidad.Mail
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    //generar una contraseña temporal
+                    string password = model.CreatePassword();
+
+                    //actualizar la contraseña temporal en la bd
+                    datos.password = password;
+                    datos.use_recovery_password = true;
+                    datos.date_recovery = DateTime.Now.AddMinutes(15);
+                    bd.SaveChanges();
+
+                    //correo
+                    StringBuilder mensaje = new StringBuilder();
+                    mensaje.Append("<html><head></head><body>");
+                    mensaje.Append("Estimado(a) " + datos.username + "<br/><br/>");
+                    mensaje.Append("Se ha generado la siguiente contraseña temporal para que ingrese al sistema: <b>" + password + "</b><br/><br/>");
+                    mensaje.Append("La contraseña generado tiene 15 minutos de validez, por favor ingrese al sistema para establecerla <br/><br/>");
+                    mensaje.Append("Este correo se ha generado de manera automática del el sistema <br/><br/>");
+                    mensaje.Append("Muchas Gracias <br/><br/>");
+                    mensaje.Append("</body></html>");
+
+                    //enviar correo
+                    model.SendEmail(datos.mail, "Recuperar Contraseña", mensaje.ToString());
+                    return true;
+                }
+
+            }
+            return false;
+        }
+
+        [HttpPut]
+        [Route("api/ChangePassword")]
+        public int ChangePassword(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Users
+                             where x.user_id == entidad.User_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.password = entidad.New_password;
+                    datos.use_recovery_password = false;
+                    datos.date_recovery = DateTime.Now;
+                    return bd.SaveChanges();
+                }
+                return 0;
+            }
+        }
+
+        [HttpPut]
+        [Route("api/UpdateUserImage")]
+        public void UpdateUserImage(UserEnt entidad)
+        {
+            using (var bd = new dbEntities())
+            {
+                var datos = (from x in bd.Users
+                             where x.user_id == entidad.User_id
+                             select x).FirstOrDefault();
+
+                if (datos != null)
+                {
+                    datos.url_image = entidad.Url_image;
+                    bd.SaveChanges();
+                }
+            }
+        }
+
+    }
+}
